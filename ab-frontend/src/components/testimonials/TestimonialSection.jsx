2b643f80b5fe4362d8609ac49571bb8eb23c03e6
Ajith K. S. <ajithks98086@gmail.com>
2025-06-14 18:52:50 +0530

    Create pagination for testimonials.

--- ab-frontend/src/components/testimonials/TestimonialSection.jsx
+++ ab-frontend/src/components/testimonials/TestimonialSection.jsx
@@ -1,4 +1,5 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Pagination, Typography } from "@mui/material";
+import { useRef, useState } from "react";
 import TestimonialCard from "./TestimonialCard";
 
 const testimonials = [
@@ -20,22 +21,55 @@ const testimonials = [
     text: "Text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content placeholder text commonly used to demonstrate the visual form of a document or typeface without relying on meaningful content.",
     image: "https://i.pravatar.cc/40?img=3",
   },
+  {
+    name: "Anderson",
+    role: "Manager / Firm",
+    text: "Text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content placeholder text commonly used to demonstrate the visual form of a document or typeface without relying on meaningful content.",
+    image: "https://i.pravatar.cc/40?img=4",
+  },
 ];
 
 export const TestimonialSection = () => {
+  const [page, setPage] = useState(1);
+  const limit = 3;
+  const totalPages = Math.ceil(testimonials.length / limit);
+  const sectionRef = useRef(null);
+
+  const handleChange = (event, value) => {
+    event.preventDefault(); // prevent default behavior, i.e., unwanted scroll to top
+    setPage(value);
+    sectionRef.current?.scrollIntoView({ behavior: "smooth" }); // scroll into view on page change
+  };
+
+  // Slice the data for current page
+  const paginatedTestimonials = testimonials.slice(
+    (page - 1) * limit,
+    page * limit
+  );
+
   return (
     <>
-      <Container sx={{ py: 5 }}>
+      <Container ref={sectionRef} sx={{ py: 5 }}>
         <Typography variant="h4" fontWeight="bold" align="left" gutterBottom>
           Testimonials
         </Typography>
         <Grid container spacing={4}>
-          {testimonials.map((item, idx) => (
+          {paginatedTestimonials.map((item, idx) => (
             <Grid size={{ xs: 12, sm: 6, md: 4 }} key={idx}>
               <TestimonialCard {...item} />
             </Grid>
           ))}
         </Grid>
+        {totalPages > 1 && (
+          <Box display="flex" justifyContent="center" mt={4}>
+            <Pagination
+              count={totalPages}
+              page={page}
+              onChange={handleChange}
+              color="primary"
+            />
+          </Box>
+        )}
       </Container>
     </>
   );
